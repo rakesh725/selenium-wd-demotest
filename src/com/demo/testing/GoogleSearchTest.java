b79ddf5b602f96c32c32fa5e36a3bfa12c879598
rakesh725 <rakesh725@gmail.com>
2012-12-13 14:46:39 +0000

    Add a package with google search test

---
+++ src/com/demo/testing/GoogleSearchTest.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.demo.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+/**
+ * @author Rakesh
+ *
+ */
+public class GoogleSearchTest {
+
+	static WebDriver driver; 
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		
+	     //Launch WebApp
+        driver = new FirefoxDriver();
+       
+	}
+
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        driver.quit();
+    	
+    }
+
+    @BeforeTest
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+    	
+    	driver.close();
+    }
+    
+
+	@Test
+	public void testSearch() {
+		
+   	 	driver.get("http://www.google.com");
+
+   	 
+		//Enter: Selenium Rocks in search
+		WebElement txSearchBar = driver.findElement(By.id("gbqfq"));
+		txSearchBar.sendKeys("Selenium Rocks");
+		//Click on Search
+		driver.findElement(By.id("gbqfb")).click();
+		
+	}
+
+}
